f1c57f5db051b0ff42b219502638f109fbbc742d
Elizabeth Beasley <roden85@gmail.com>
2022-06-28 22:54:15 -0400

    initial js commit

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+let places = {
+  yosemiteValley: {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  lakeLouise: {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  baldMountains: {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  latemar: {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  vanoiseNationalPark: {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  lagoDiBraies: {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+};
+
+let initialCards = [];
+
+initialCards.push(places);

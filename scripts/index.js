9d508fbfd7ae4f0796dfffcaf44f1d8f2591d457
Elizabeth Beasley <roden85@gmail.com>
2022-08-14 20:39:08 -0400

    refactor: file path

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-import FormValidator from "./FormValidator.js";
-import Card from "./Card.js";
+import FormValidator from "./formvalidator.js";
+import Card from "./card.js";
 import { openPopup, closePopup } from "./utils.js";
 
 // constants
